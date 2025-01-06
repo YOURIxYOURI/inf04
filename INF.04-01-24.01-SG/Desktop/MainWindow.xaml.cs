de17b81d8b8e56cdd8ff91e1ae2f92837ec872cf
YouriProgrammer <97059905+YOURIxYOURI@users.noreply.github.com>
2025-01-06 10:41:56 +0100

    trying multiple windows in wpf

--- INF.04-01-24.01-SG/Desktop/MainWindow.xaml.cs
+++ INF.04-01-24.01-SG/Desktop/MainWindow.xaml.cs
@@ -1,6 +1,4 @@
 ﻿using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media.Imaging;
 
 namespace Desktop
 {
@@ -12,42 +10,10 @@ namespace Desktop
         public MainWindow()
         {
             InitializeComponent();
+            this.contentControl.Content = new UserControl1(this.contentControl);
         }
 
-        private void numberEntry_LostFocus(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                var entry = (TextBox)sender;
 
-                string url = $@"/{entry.Text.ToString()}-zdjecie.jpg";
 
-                personImage.Source = new BitmapImage(new Uri(url, UriKind.Relative));
-
-                url = $@"/{entry.Text.ToString()}-odcisk.jpg";
-
-                printImage.Source = new BitmapImage(new Uri(url, UriKind.Relative));
-            }
-            catch (Exception ex) { }
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            if (!string.IsNullOrEmpty(firstNameEntry.Text.ToString()) && !string.IsNullOrEmpty(lastNameEntry.Text.ToString()))
-            {
-                string eyes = "";
-                if (greenCheckbox.IsChecked == true)
-                    eyes = "zielone";
-                else if (blueCheckbox.IsChecked == true)
-                    eyes = "niebieskie";
-                else if (beerCheckbox.IsChecked == true)
-                    eyes = "piwne";
-                MessageBox.Show($"{firstNameEntry.Text.ToString()} {lastNameEntry.Text.ToString()} kolor oczu {eyes}");
-            }
-            else
-            {
-                MessageBox.Show("Wprowadź dane");
-            }
-        }
     }
 }
